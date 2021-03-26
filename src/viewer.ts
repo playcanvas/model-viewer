c03b40c01ea08968bef93d997828a2bd84dd8063
Donovan Hutchence <slimbuck7@gmail.com>
2021-03-26 15:27:01 +0000

    Remove hdr-texture parser (#103)
    * remove hdr-texture parser

--- src/viewer.ts
+++ src/viewer.ts
@@ -4,8 +4,6 @@ import * as pcx from 'playcanvas/build/playcanvas-extras.js';
 import Graph from './graph';
 import DebugLines from './debug';
 // @ts-ignore: library file import
-import HdrParser from 'lib/hdr-texture.js';
-// @ts-ignore: library file import
 import * as MeshoptDecoder from 'lib/meshopt_decoder.js';
 import { getAssetPath } from './helpers';
 import { Morph, URL, Entry, Observer, HierarchyNode } from './types';
@@ -176,10 +174,6 @@ class Viewer {
         this.miniStats = new pcx.MiniStats(app);
         this.miniStats.enabled = observer.get('show.stats');
         this.observer = observer;
-
-        // initialize the envmap
-        // @ts-ignore: Missing pc definition
-        app.loader.getHandler(pc.ASSET_TEXTURE).parsers.hdr = new HdrParser(app.assets, false);
 
         // initialize control events
         this.bindControlEvents();
