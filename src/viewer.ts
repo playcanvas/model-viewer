ff61445f47a48b2c8b569ba8c625cd384d2208f7
Will Eastcott <will@playcanvas.com>
2021-11-03 09:18:10 +0000

    Lint fixes

--- src/viewer.ts
+++ src/viewer.ts
@@ -288,7 +288,7 @@ class Viewer {
         result.setMinMax(new pc.Vec3(Math.max(min1.x, min2.x), Math.max(min1.y, min2.y), Math.max(min1.z, min2.z)),
                          new pc.Vec3(Math.min(max1.x, max2.x), Math.min(max1.y, max2.y), Math.min(max1.z, max2.z)));
         return result;
-    }
+    };
 
     // construct the controls interface and initialize controls
     private bindControlEvents() {
@@ -709,7 +709,7 @@ class Viewer {
 
         // step through urls loading gltf/glb models
         let result = false;
-        urls.forEach((url) =>  {
+        urls.forEach((url) => {
             const filenameExt = pc.path.getExtension(url.filename).toLowerCase();
             if (filenameExt === '.gltf' || filenameExt === '.glb') {
                 this.loadGltf(url, urls);
