1d12c42462ac994384cb80db3ccf53ad82fa4c84
Elliott Thompson <ellth92@gmail.com>
2022-01-20 15:26:13 +0000

    fix an issue with the server build of the viewer

--- rollup.config.js
+++ rollup.config.js
@@ -38,7 +38,7 @@ export default {
         }),
         replace({
             'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-            '__PUBLIC_PATH__': process.env.PUBLIC_PATH
+            '__PUBLIC_PATH__': JSON.stringify(process.env.PUBLIC_PATH)
         }),
         copy({
             targets: [
