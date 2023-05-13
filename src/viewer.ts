9692d2e3025f86d60d2ed5c9ff218f2bbfcf675d
Donovan Hutchence <slimbuck7@gmail.com>
2023-05-13 10:48:05 +0100

    lint

--- src/viewer.ts
+++ src/viewer.ts
@@ -937,23 +937,23 @@ class Viewer {
             if (gltfBuffer.extensions && gltfBuffer.extensions.EXT_meshopt_compression) {
                 const extensionDef = gltfBuffer.extensions.EXT_meshopt_compression;
 
-                Promise.all([ MeshoptDecoder.ready, buffers[extensionDef.buffer] ])
-                .then((promiseResult) => {
-                    const buffer = promiseResult[1];
+                Promise.all([MeshoptDecoder.ready, buffers[extensionDef.buffer]])
+                    .then((promiseResult) => {
+                        const buffer = promiseResult[1];
 
-                    const byteOffset = extensionDef.byteOffset || 0;
-                    const byteLength = extensionDef.byteLength || 0;
+                        const byteOffset = extensionDef.byteOffset || 0;
+                        const byteLength = extensionDef.byteLength || 0;
 
-                    const count = extensionDef.count;
-                    const stride = extensionDef.byteStride;
+                        const count = extensionDef.count;
+                        const stride = extensionDef.byteStride;
 
-                    const result = new Uint8Array(count * stride);
-                    const source = new Uint8Array(buffer.buffer, buffer.byteOffset + byteOffset, byteLength);
+                        const result = new Uint8Array(count * stride);
+                        const source = new Uint8Array(buffer.buffer, buffer.byteOffset + byteOffset, byteLength);
 
-                    MeshoptDecoder.decodeGltfBuffer(result, count, stride, source, extensionDef.mode, extensionDef.filter);
+                        MeshoptDecoder.decodeGltfBuffer(result, count, stride, source, extensionDef.mode, extensionDef.filter);
 
-                    continuation(null, result);
-                });
+                        continuation(null, result);
+                    });
             } else {
                 continuation(null, null);
             }
