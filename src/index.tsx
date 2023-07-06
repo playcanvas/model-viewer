c1933c9d2407b807cb1f6d3576d7f45ef0c53f06
Donovan Hutchence <slimbuck7@gmail.com>
2023-07-06 14:57:37 +0100

    Store shadowCatcher settings (#226)

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import {
     basisInitialize,
-    shaderChunks,
     Vec3,
     WasmModule
 } from 'playcanvas';
@@ -28,44 +27,6 @@ declare global {
         webkit?: any;
     }
 }
-
-shaderChunks.debugOutputPS = `
-#ifdef DEBUG_ALBEDO_PASS
-gl_FragColor = vec4(gammaCorrectOutput(litShaderArgs.albedo), 1.0);
-#endif
-
-#ifdef DEBUG_UV0_PASS
-gl_FragColor = vec4(litShaderArgs.albedo, 1.0);
-#endif
-
-#ifdef DEBUG_WORLD_NORMAL_PASS
-gl_FragColor = vec4(litShaderArgs.worldNormal * 0.5 + 0.5, 1.0);
-#endif
-
-#ifdef DEBUG_OPACITY_PASS
-gl_FragColor = vec4(vec3(litShaderArgs.opacity) , 1.0);
-#endif
-
-#ifdef DEBUG_SPECULARITY_PASS
-gl_FragColor = vec4(litShaderArgs.specularity, 1.0);
-#endif
-
-#ifdef DEBUG_GLOSS_PASS
-gl_FragColor = vec4(vec3(litShaderArgs.gloss) , 1.0);
-#endif
-
-#ifdef DEBUG_METALNESS_PASS
-gl_FragColor = vec4(vec3(litShaderArgs.metalness) , 1.0);
-#endif
-
-#ifdef DEBUG_AO_PASS
-gl_FragColor = vec4(gammaCorrectOutput(vec3(litShaderArgs.ao)), 1.0);
-#endif
-
-#ifdef DEBUG_EMISSION_PASS
-gl_FragColor = vec4(litShaderArgs.emission, 1.0);
-#endif
-`;
 
 interface Skybox {
     url: string,
@@ -188,7 +149,8 @@ const saveOptions = (name: string) => {
         camera: options.camera,
         skybox: options.skybox,
         light: options.light,
-        debug: options.debug
+        debug: options.debug,
+        shadowCatcher: options.shadowCatcher
     }));
 };
 
