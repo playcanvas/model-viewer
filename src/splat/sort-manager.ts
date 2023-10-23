9eb003910c0b02e276e4d3eba884e0ad22736fc9
Martin Valigursky <59932779+mvaligursky@users.noreply.github.com>
2023-10-23 12:05:42 +0100

    Use radix sort with 16bit keys to sort splats (#259)
    * Use radix sort with 16bit keys to sort splats
    
    * updated comment
    
    * higher base for radix
    
    * comment
    
    * sort bound
    
    * lint
    
    * lint
    
    * remove console log
    
    * dont generate new buffer every sort
    
    ---------
    
    Co-authored-by: Martin Valigursky <mvaligursky@snapchat.com>
    Co-authored-by: Donovan Hutchence <slimbuck7@gmail.com>

--- src/splat/sort-manager.ts
+++ src/splat/sort-manager.ts
@@ -10,6 +10,15 @@ type Vec3 = {
 function SortWorker() {
     const epsilon = 0.0001;
 
+    // number of bits used to store the distance in integer array. Smaller number gives it a smaller
+    // precision but less radix sort passes to sort. Could even be dynamic for less precise sorting.
+    // 16bit seems plenty of large scenes (train), 10bits is enough for sled.
+    const compareBits = 16;
+
+    // larger based makes a lot less passes by radix sort, but each pass is slightly slower. Big win
+    // to use 512 vs 10. Needs to find a the sweet spot for this.
+    const radixBase = 512;
+
     let data: Float32Array;
     let centers: Float32Array;
     let cameraPosition: Vec3;
@@ -24,7 +33,49 @@ function SortWorker() {
 
     let orderBuffer: BigUint64Array;
     let orderBuffer32: Uint32Array;
+    let orderBufferTmp: BigUint64Array;
     let target: Float32Array;
+
+    // A function to do counting sort of arr[] according to the digit represented by exp.
+    const countSort = (arr: BigUint64Array, arr32: Uint32Array, temp: BigUint64Array, n: number, exp: number) => {
+        const count = new Array(radixBase);
+        for (let i = 0; i < radixBase; i++)
+            count[i] = 0;
+
+        // Store count of occurrences in count[]
+        for (let i = 0; i < n; i++) {
+            const x = Math.floor(arr32[i * 2 + 1] / exp) % radixBase;
+            count[x]++;
+        }
+
+        // Change count[i] so that count[i] now contains actual position of this digit in output[]
+        for (let i = 1; i < radixBase; i++)
+            count[i] += count[i - 1];
+
+        // Build the output array
+        for (let i = n - 1; i >= 0; i--) {
+            const x = Math.floor(arr32[i * 2 + 1] / exp) % radixBase;
+            temp[count[x] - 1] = arr[i];
+            count[x]--;
+        }
+
+        // Copy the output array to arr[], so that arr[] now contains sorted numbers according to current digit
+        for (let i = 0; i < n; i++)
+            arr[i] = temp[i];
+    };
+
+    // The main function to that sorts arr[] of size n using Radix Sort
+    const radixSort = (arr: BigUint64Array, arr32: Uint32Array, arrTmp: BigUint64Array, n: number) => {
+
+        // maximum number to know number of digits
+        const m = 2 ** compareBits;
+
+        // Do counting sort for every digit. Note that instead of passing digit number, exp is passed.
+        // exp is 10^i where i is current digit number
+        for (let exp = 1; Math.floor(m / exp) > 0; exp *= radixBase) {
+            countSort(arr, arr32, arrTmp, n, exp);
+        }
+    };
 
     const update = () => {
         if (!centers || !data || !cameraPosition || !cameraDirection) return;
@@ -59,6 +110,7 @@ function SortWorker() {
         if (orderBuffer?.length !== numVertices) {
             orderBuffer = new BigUint64Array(numVertices);
             orderBuffer32 = new Uint32Array(orderBuffer.buffer);
+            orderBufferTmp = new BigUint64Array(numVertices);
             target = new Float32Array(numVertices);
         }
 
@@ -86,11 +138,18 @@ function SortWorker() {
                       (centers[istride + 1] - py) * dy +
                       (centers[istride + 2] - pz) * dz;
             orderBuffer32[i * 2 + 0] = i;
-            orderBuffer32[i * 2 + 1] = Math.floor((d - minDist) / range * (2 ** 32));
+            orderBuffer32[i * 2 + 1] = Math.floor((d - minDist) / range * (2 ** compareBits));
         }
 
-        // sort indices
-        orderBuffer.sort();
+        // console.time("sort");
+
+        // sort indices by distance only, so use distance in orderBuffer32 as sorting key
+        radixSort(orderBuffer, orderBuffer32, orderBufferTmp, numVertices);
+
+        // normal sort
+        // orderBuffer.sort();
+
+        // console.timeEnd("sort");
 
         // order the splat data
         if (intIndices) {
