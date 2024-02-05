dc5147bcbb2a3421d3bf5f6043f76e070974794e
Donovan Hutchence <slimbuck7@gmail.com>
2024-02-05 13:44:12 +0000

    remove dead code

--- src/app.ts
+++ src/app.ts
@@ -1,57 +1,17 @@
 import {
-    // SoundManager,
     Lightmapper,
-    // BatchManager,
     AppBase,
     AppOptions,
-    // script,
-    // AnimationComponentSystem,
     AnimComponentSystem,
-    // AudioListenerComponentSystem,
-    // AudioSourceComponentSystem,
-    // ButtonComponentSystem,
-    // CollisionComponentSystem,
-    // ElementComponentSystem,
-    // JointComponentSystem,
-    // LayoutChildComponentSystem,
-    // LayoutGroupComponentSystem,
-    // ModelComponentSystem,
-    // ParticleSystemComponentSystem,
     RenderComponentSystem,
-    // RigidBodyComponentSystem,
-    // ScreenComponentSystem,
-    // ScriptLegacyComponentSystem,
-    // ScrollViewComponentSystem,
-    // ScrollbarComponentSystem,
-    // SoundComponentSystem,
-    // SpriteComponentSystem,
-    // ZoneComponentSystem,
     CameraComponentSystem,
     LightComponentSystem,
-    // ScriptComponentSystem,
     RenderHandler,
-    // AnimationHandler,
     AnimClipHandler,
     AnimStateGraphHandler,
-    // AudioHandler,
     BinaryHandler,
     ContainerHandler,
-    // CssHandler,
     CubemapHandler,
-    // FolderHandler,
-    // FontHandler,
-    // HierarchyHandler,
-    // HtmlHandler,
-    // JsonHandler,
-    // MaterialHandler,
-    // ModelHandler,
-    // SceneHandler,
-    // ScriptHandler,
-    // ShaderHandler,
-    // SpriteHandler,
-    // TemplateHandler,
-    // TextHandler,
-    // TextureAtlasHandler,
     TextureHandler,
     XrManager,
     GSplatComponentSystem,
@@ -78,10 +38,8 @@ class App extends AppBase {
         appOptions.assetPrefix = options.assetPrefix;
         appOptions.scriptsOrder = options.scriptsOrder;
 
-        // appOptions.soundManager = new SoundManager(options);
         // @ts-ignore
         appOptions.lightmapper = Lightmapper;
-        // appOptions.batchManager = BatchManager;
         // @ts-ignore
         appOptions.xr = XrManager;
 
@@ -90,33 +48,10 @@ class App extends AppBase {
 
     addComponentSystems(appOptions: AppOptions) {
         appOptions.componentSystems = [
-            // RigidBodyComponentSystem,
-            // CollisionComponentSystem,
-            // JointComponentSystem,
-            // AnimationComponentSystem,
-            // @ts-ignore
             AnimComponentSystem,
-            // ModelComponentSystem,
-            // @ts-ignore
             RenderComponentSystem,
-            // @ts-ignore
             CameraComponentSystem,
-            // @ts-ignore
             LightComponentSystem,
-            // script.legacy ? ScriptLegacyComponentSystem : ScriptComponentSystem,
-            // AudioSourceComponentSystem,
-            // SoundComponentSystem,
-            // AudioListenerComponentSystem,
-            // ParticleSystemComponentSystem,
-            // ScreenComponentSystem,
-            // ElementComponentSystem,
-            // ButtonComponentSystem,
-            // ScrollViewComponentSystem,
-            // ScrollbarComponentSystem,
-            // SpriteComponentSystem,
-            // LayoutGroupComponentSystem,
-            // LayoutChildComponentSystem,
-            // ZoneComponentSystem,
             GSplatComponentSystem
         ];
     }
@@ -125,35 +60,19 @@ class App extends AppBase {
         appOptions.resourceHandlers = [
             // @ts-ignore
             RenderHandler,
-            // AnimationHandler,
             // @ts-ignore
             AnimClipHandler,
             // @ts-ignore
             AnimStateGraphHandler,
-            // ModelHandler,
-            // MaterialHandler,
             // @ts-ignore
             TextureHandler,
-            // TextHandler,
-            // JsonHandler,
-            // AudioHandler,
-            // ScriptHandler,
-            // SceneHandler,
             // @ts-ignore
             CubemapHandler,
-            // HtmlHandler,
-            // CssHandler,
-            // ShaderHandler,
-            // HierarchyHandler,
-            // FolderHandler,
-            // FontHandler,
             // @ts-ignore
             BinaryHandler,
-            // TextureAtlasHandler,
-            // SpriteHandler,
-            // TemplateHandler,
             // @ts-ignore
             ContainerHandler,
+            // @ts-ignore
             GSplatHandler
         ];
     }
